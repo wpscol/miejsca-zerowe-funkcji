44437552ab4292cecda249bbd5a9a48be8e98462
wpscol <witoldp.school@protonmail.com>
2022-10-24 11:25:13 +0200

    1.2
    a = 0, can now be provided

--- main.cpp
+++ main.cpp
@@ -5,13 +5,21 @@ auto main() -> int {
   double a = 0;
   double b = 0;
 
-  std::cout << "\n[Miejsca zerowe funkcji]\nObliczanie miejsc zerowych funkcji liniowej  (ax + b = 0):\n  [a != 0] Podaj [a]: ";
+  std::cout << "\n\nObliczanie miejsc zerowych funkcji liniowej (ax + b = 0):\n  Podaj [a]: ";
   std::cin >> a;
   std::cout << "  Podaj [b]: ";
   std::cin >> b;
 
-  double x0 = ((-1)*b)/a;
-  std::cout << "\nX0 = " << x0;
+  if (a == 0 && b != 0)
+    std::cout << "\nBrak miejsca zerowego.";
+
+  else if (a == 0 && b == 0)
+    std::cout << "\nWszystkie liczby rzeczywiste są miejscami zerowymi funkcji.";
+    
+  else {
+    double x0 = ((-1)*b)/a;
+      std::cout << "\nX0 = " << x0;
+  }
 
   return 0;
 }
